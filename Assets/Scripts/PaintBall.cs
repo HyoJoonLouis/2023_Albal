cad1f66609f1c46807fc43ec74ee0c9d30f4c749
HyoJoon <40314883+HyoJoonLouis@users.noreply.github.com>
2023-08-11 02:41:38 +0900

    123

--- Assets/Scripts/PaintBall.cs
+++ Assets/Scripts/PaintBall.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class PaintBall : MonoBehaviour
 {
-    private float Speed;
-    private Vector3 Direction;
+
     private int Bounce;
+    private Rigidbody rb;
+    private Vector3 PreviousVelocity;
 
     [Header("Shader")]
     [SerializeField] Texture2D[] PaintBrush;
@@ -20,17 +21,17 @@ public class PaintBall : MonoBehaviour
     private void Start()
     {
         Bounce = 0;
+        rb = GetComponent<Rigidbody>();
     }
 
     public void Init(float speed, Vector3 direction, Vector3 position)
     {
         this.transform.position = position;
-        Speed = speed;
-        Direction = direction.normalized;
-        Rigidbody rb = GetComponent<Rigidbody>();
 /*        rb.velocity = new Vector3(0, 0, 0);
         rb.AddForce(Speed * Direction, ForceMode.Impulse);*/
-        rb.velocity = Speed * direction;
+        rb.velocity = speed * direction.normalized;
+        Debug.Log("Init direction" + direction);
+        Debug.Log("Init Velocity" + rb.velocity);
     }
     
     public void SetBounce(int value)
@@ -38,17 +39,19 @@ public class PaintBall : MonoBehaviour
         Bounce = value;
     }
 
+    public void LateUpdate()
+    {
+        PreviousVelocity = rb.velocity;
+    }
+
     private void OnCollisionEnter(Collision collision)
     {
         if(Bounce > 0)
         {
             Bounce--;
-            Rigidbody rb = this.GetComponent<Rigidbody>();
-            Debug.Log(rb.velocity);
-            float speed = rb.velocity.magnitude;
-            Vector3 direction = Vector3.Reflect(rb.velocity.normalized, collision.contacts[0].normal);
+            float speed = PreviousVelocity.magnitude;
+            Vector3 direction = Vector3.Reflect(PreviousVelocity.normalized, collision.contacts[0].normal);
             rb.velocity = direction * speed * decreaseSpeedAmount;
-            Debug.Log(rb.velocity);
             return;
         }
 
